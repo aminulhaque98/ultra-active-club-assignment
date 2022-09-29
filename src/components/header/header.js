859aec41b983cfd74d9a658c3496630d8e269f5c
aminulhaque98 <aminulhaque419@gmail.com>
2022-09-29 06:05:03 +0530

    normaly header set

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div>
+            <h1>UtRA-Active-club</h1>
+            <p>Select today’s exercise</p>
+        </div>
+    );
+};
+
+export default header;
